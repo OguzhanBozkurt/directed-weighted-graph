46c0da078b9a4429bf9a91f171c7dd4f98b15197
OguzhanBozkurt <oguzhan.bozkurt.28@gmail.com>
2021-08-23 15:45:19 +0300

    Add Graph and WeightedGraph documentations

--- src/include/Graph.h
+++ src/include/Graph.h
@@ -6,12 +6,55 @@
 
 class Graph {
 public:
+    /**
+     * Creates an empty graph object
+     */
     Graph();
+
+    /**
+     * Creates a graph object from an existing graph
+     * 
+     * @param[in] graph Input graph which will be copied
+     */
     Graph(const Graph& graph);
+
     ~Graph();
+
+    /**
+     * Adds a node to the graph
+     * 
+     * @param[in] node  Pointer to the node which will be added to this graph 
+     * 
+     * @return  false   : If node is already added
+     *          true    : Otherwise
+     */
     bool AddNode(Node* node);
+
+    /**
+     * Deletes a node from the graph
+     * 
+     * @param[in] node  Pointer to the node which will be deleted from this graph 
+     * 
+     * @return  false   : If node is not added with AddNode beforehand
+     *          true    : Otherwise
+     */
     bool DeleteNode(Node* node);
+
+    /**
+     * Returns whether the graph is acyclic or not
+     * 
+     * Acyclic graph does not contain any cycles in it.
+     * 
+     * @return  false   : If the graph is cyclic
+     *          true    : If the graph is acyclic
+     */
     bool IsAcyclic(void);
+
+    /**
+     * Returns the count of the nodes on the graph
+     * 
+     * @return  count of the nodes
+     */
     int Size(void);
 
 protected:
